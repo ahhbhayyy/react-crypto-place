1bab04036d0747073d92c65b267534508be41346
ahhbhayyy <lesnar.abhay@gmail.com>
2025-09-08 14:06:22 +0530

    mthrene

---
+++ src/components/Navbar/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Footer.css'
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright @2025,Cryptoplace - All Right Reserved.</p>
+    </div>
+  )
+}
